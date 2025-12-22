012f1d222a048a53810fa8ea1cc42d6e5a7f36a2
tan9799 <tan90_0320@qq.com>
2025-12-22 14:36:48 +0800

    C语言

---
+++ 8-8 判断回文字符串.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXN 20
+typedef enum {false, true} bool;
+
+bool palindrome( char *s );
+
+int main()
+{
+    char s[MAXN];
+
+    scanf("%s", s);
+    if ( palindrome(s)==true )
+        printf("Yes\n");
+    else
+        printf("No\n");
+    printf("%s\n", s);
+
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+bool palindrome( char *s )
+{
+    int len = strlen(s);
+    int left = 0;
+    int right = len - 1;
+    while(right > left)
+    {
+        if(s[left] != s[right])
+        {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
