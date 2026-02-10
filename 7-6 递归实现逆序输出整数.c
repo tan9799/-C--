917d95bc9cef3b8d18fe60dc2dbbc9244f849574
tan9799 <tan90_0320@qq.com>
2026-02-10 17:18:34 +0800

    C语言

---
+++ 7-6 递归实现逆序输出整数.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void reverse(int n)
+{
+	while(n != 0)
+	{
+		printf("%d", n % 10);
+		n = n / 10;
+    }
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	reverse(n);
+	return 0;
+}
